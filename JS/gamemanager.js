d4fcfb0e7910b3951739e0f1847beaa32490918b
taniyaamidon <harshanitaniya@yahoo.co.uk>
2018-11-23 14:35:49 +0000

    completed

--- JS/gamemanager.js
+++ JS/gamemanager.js
@@ -18,7 +18,7 @@ let GameManager = {
         }
 
         let getInterface = document.querySelector(".interface");
-        getInterface.innerHTML = '<img src="images/' + classType.toLowerCase() + '.png" + class="img-avatar"><div><h3>' + classType + '</h3><p>Health: ' + player.health + '</p>' + '<p>Mana: ' + player.mana + '</p>' + '<p>Strength: ' + player.strength + '</p>' + '<p>Aglility: ' + player.agility + '</p>' + '<p>Speed: ' + player.speed + '</p></div>'; 
+        getInterface.innerHTML = '<img src="images/' + classType.toLowerCase() + '.png" + class="img-avatar"><div><h3>' + classType + '</h3><p class=".health-player">Health: ' + player.health + '</p>' + '<p>Mana: ' + player.mana + '</p>' + '<p>Strength: ' + player.strength + '</p>' + '<p>Aglility: ' + player.agility + '</p>' + '<p>Speed: ' + player.speed + '</p></div>'; 
     },
 
     setPreFight: function() {
@@ -37,11 +37,23 @@ let GameManager = {
         let getActions = document.querySelector(".actions");
         let getEnemy = document.querySelector(".enemy");
         // create enemy
-        let enemy00 = new Enemy("Evil Cat", 100, 0, 50, 100, 100);
+        let enemy00 = new Enemy("Evil-Cat", 100, 0, 50, 100, 100);
         let enemy01 = new Enemy("Dragon", 200, 0, 150, 80, 100);
 
         let chooseRandomEnemy = Math.floor(Math.random() * Math.floor(2));
+            switch (chooseRandomEnemy) {
+                case 0:
+                    enemy = enemy00;
+                    break;
+                case 1:
+                    enemy = enemy01;
+                break;
+            }
 
-    }
+        getHeader.innerHTML ='<p>Task: Choose your move!</p>';
+        getActions.innerHTML ='<a href="#" class="btn-prefight" onclick="PlayerMoves.calcAttack()">Attack!</a>';
+        //enemy
+        getEnemy.innerHTML = '<img src="images/' + enemy.enemyType.toLowerCase() + '.png" + class="img-avatar"><div><h3>' + enemy.enemyType + '</h3><p class="health-enemy">Health: ' + enemy.health + '</p>' + '<p>Mana: ' + enemy.mana + '</p>' + '<p>Strength: ' + enemy.strength + '</p>' + '<p>Aglility: ' + enemy.agility + '</p>' + '<p>Speed: ' + enemy.speed + '</p></div>'; 
+        }
 
 }
